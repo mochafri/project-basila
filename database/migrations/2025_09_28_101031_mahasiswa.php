36bb0bb6240bd846827b5991021d95c8f656ac56
ilhom <rilhamgojali123@gmail.com>
2025-10-23 20:34:37 +0700

    Update bagian migrasi dan seeder juga

--- database/migrations/2025_09_28_101031_mahasiswa.php
+++ database/migrations/2025_09_28_101031_mahasiswa.php
@@ -4,27 +4,26 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    // public function up(): void
-    // {
-    //     Schema::create('mahasiswa', function (Blueprint $table) {
-    //         $table->id();
-    //         $table->string('nim')->unique();
-    //         $table->integer('fakultas_id');
-    //         $table->integer('prody_id');
-    //         $table->string('name');
-    //         $table->integer('study_period');
-    //         $table->integer('pass_sks');
-    //         $table->decimal('ipk', 3, 2);
-    //         $table->string('predikat')->nullable();
-    //         $table->string('status')->nullable();
-    //         $table->timestamps();
-    //     });
-    // }
+    public function up(): void
+    {
+        Schema::create('mahasiswa', function (Blueprint $table) {
+            $table->id();
+            $table->string('STUDENTID')->unique();
+            $table->string('FULLNAME');
+            $table->string('MASA_STUDI');
+            $table->integer('PASS_CREDIT');
+            $table->decimal('GPA', 3, 2);
+            $table->string('STATUS')->nullable();
+            $table->integer('STUDYPROGRAMID');
+            $table->integer('FACULTYID');
+            $table->string('PREDIKAT')->nullable();
+            $table->timestamps();
+        });
+    }
 
     /**
      * Reverse the migrations.
