4d704e5657a8c9fb285f60cf40496f6477c8a830
Mochammad Tontowi Jauhari <tontowijauhari285@gmail.com>
2026-02-03 08:35:15 +0700

    mengaktifkan kembali footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,5 @@
-<footer class="d-footer">
-    <!-- <div class="flex items-center justify-between gap-3">
-        <p class="mb-0">© 2025 WowDash. All Rights Reserved.</p>
-        <p class="mb-0">Made by <span class="text-primary-600">Pixcels Themes</span></p>
-    </div> -->
+<footer class="">
+    <div class="flex items-center justify-between gap-3 text-sm text-neutral-500">
+        <p class="mb-0">Copyright © iBasila Digital Service</p>
+    </div>
 </footer>
